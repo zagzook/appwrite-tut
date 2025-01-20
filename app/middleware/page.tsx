94898a584528cf922bf4e1d0083ebb59577484aa
zagzook <ja0213@gmail.com>
2025-01-20 10:07:28 -0500

    removed middleware

--- app/middleware/page.tsx
+++ app/middleware/page.tsx
@@ -1,8 +1,11 @@
 import { getLoggedInUser } from '@/actions/auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Middleware = async () => {
   const user: userDetails | null = await getLoggedInUser()
+  if (!user?.proMember) redirect('/')
+  console.log('user', user)
   return (
     <main className="flex flex-col h-full items-center justify-center gap-2">
       <h1 className="text-3xl">Middleware Page</h1>
