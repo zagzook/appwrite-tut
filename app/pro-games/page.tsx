b521903f4ace183074b2cb0ef8ad1e886308705c
zagzook <ja0213@gmail.com>
2025-01-20 09:41:11 -0500

    removed possible conflicted code for middleware error

--- app/pro-games/page.tsx
+++ app/pro-games/page.tsx
@@ -1,10 +1,8 @@
 import { getLoggedInUser } from '@/actions/auth'
-import { redirect } from 'next/navigation'
 import React from 'react'
 
 const ProGames = async () => {
   const user: userDetails | null = await getLoggedInUser()
-  if (!user?.proMember) redirect('/')
   return (
     <main className="flex flex-col h-full items-center justify-center gap-2">
       <h1 className="text-3xl">Pro Games Page</h1>
